6c27d6aa1ef5870a68bc1d2410401f6b6f028401
harish0916 <harish0916ojhacs@gmail.com>
2024-08-27 16:25:51 +0530

    operator

--- PROJ4/script.js
+++ PROJ4/script.js
@@ -49,6 +49,105 @@
 // console.log(greeting("Ravi"))
 
 
-let sum = (a,b) => a+b
+// let sum = (a,b) => a+b
 
-console.log(sum(200,300))
+// console.log(sum(200,300))
+
+// ---------------
+
+// IIF
+
+// (function msg(){     // function definition
+//     console.log("Happy Janmashtami");
+// })()
+
+// (function (a,b){
+//     console.log(`sum of ${a} and ${b} is ${a+b}`);
+// })(12,13)
+
+// let msg = (function(){     // function definition
+//     return "Happy Janmashtami";
+// })()
+
+// console.log(msg);
+
+// ------------------------
+
+
+// function *myGen(){
+//     yield 1
+//     yield 2
+//     yield 3
+//     yield 4
+// }
+
+// const g = myGen()
+// console.log(g.next().value);
+// console.log(g.next().value);
+// console.log(g.next().value);
+// console.log(g.next().value);
+// console.log(g.next().value);
+
+
+// function *myGen(){
+//     for(let i=1; i<=10; i++){
+//         yield i;
+//     }
+// }
+
+// const g = myGen()
+// for(let i=1; i<=10; i++){
+//     console.log(g.next().value);
+// }
+
+
+// ---------------
+// let a = 5
+// console.log(a++ + ++a);   
+
+//--------------------
+
+// logical &&
+
+// console.log( "hello" && "welcome"); // welcome
+// console.log( "" && "welcome");  // blank
+// console.log( "hello" && "");    // blank
+// console.log(12 && 13);      // 13
+// console.log(null && 13);    // null
+
+// let a = 12
+// console.log(a>10 && "a is greater than 10")
+//                 // a is greater than 10
+
+// console.log("hello")    // hello
+// console.log(!"hello")   // false
+// console.log(!!"hello")  // true
+
+
+// OR Operator
+
+// console.log( "hello" || "welcome"); // hello
+// console.log( "" || "welcome");  // welcome
+// console.log( "hello" || "");    // hello
+// console.log(12 || 13);      // 12
+// console.log(null || 13);    // 13
+
+
+// let a = 12
+// console.log(a>10 || "a is greater than 10")
+//                 // true
+
+
+// /---------------------
+
+// Ternary operator
+
+let a=10
+
+let show = a>5 ? "a is greater than 5": "a is not greater than 5"
+
+console.log(show);
+
+
+
+
