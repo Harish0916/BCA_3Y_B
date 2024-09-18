76759c69fdd3e6ed377b50c1e21017e07163085f
harish0916 <harish0916ojhacs@gmail.com>
2024-09-18 15:03:57 +0530

    dom update

---
+++ PROJ7_DOM/script.js
@@ -0,0 +1,26 @@
+let x = document.querySelector("h1")
+console.log(x.textContent);
+x.textContent = "welcome changes"
+
+let x = document.querySelector("#did")
+console.log(x.textContent);
+x.textContent = "welcome id changes"
+console.log(x.textContent);
+
+let x = document.querySelector(".dclass")
+console.log(x.textContent);
+x.textContent = "welcome class changes"
+console.log(x.textContent);
+
+let x = document.querySelector(".dclass")
+console.log(x.textContent);
+x.textContent = "welcome class changes"
+console.log(x.textContent);
+
+let x = document.querySelector("div p")
+console.log(x.textContent);
+x.textContent = "welcome div p changes"
+console.log(x.textContent);
+
+let x = document.querySelector("input[type]")
+console.log(x.type);
