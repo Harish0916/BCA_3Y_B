a3ef77515ad9754d3ce8a91f8344d2069d8434d4
harish0916 <harish0916ojhacs@gmail.com>
2024-09-19 12:00:55 +0530

    dom + event

---
+++ PROJ7_DOM/script2.js
@@ -0,0 +1,10 @@
+// let content = document.getElementById('menu');
+// let firstChild = content.firstChild.nodeName;
+// console.log(firstChild);
+
+// let menu = document.getElementById('menu');
+// console.log(menu.lastElementChild);
+
+let menu = document.getElementById('menu');
+let children = menu.children;
+console.log(children);
